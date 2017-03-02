31fe886fb14327b5039bd49c0155cf3bd2e8047b
Владислав Яковлев <vlad309523@gmail.com>
2017-03-02 17:26:22 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,178 @@
+# Simple gulp
+
+
+## Gulp tasks
+
+### build
+
+Build project
+
+### watch
+
+Watch for changes
+
+### clean
+
+Clean destination folder
+
+
+## API
+
+```
+require('gulp-simple')(config, options);
+```
+
+### config
+
+Type: `Object`
+
+Required
+
+### config.src
+
+Type: `String`
+
+Required
+
+Source directory
+
+### config.dest
+
+Type: `String`
+
+Required
+
+Destination directory
+
+### config.types
+
+Type: `Object`
+
+Required
+
+List of types
+
+### config.types[type].src
+
+**Type**: `String`
+
+Required
+
+Filter souce relative `config.src`
+
+### config.types[type].dest
+
+Type: `String`
+
+Default: `''`
+
+Destination folder relative `config.dest`
+
+### config.types[type].pipe
+
+Type: `Array`
+
+Default: empty pipe
+
+List of pipes
+
+### config.types[type].pipe[n]
+
+Type: `Array of String`
+
+Required
+
+First is name of npm module, Following are arguments
+
+### config.types[type].minify
+
+See `config.types[type].pipe`
+
+Uses when `options.minify` is `true`
+
+### config.clean
+
+Type: `String` `Array of String`
+
+Default: `config.dest`
+
+Argument for [del](https://www.npmjs.com/package/del)
+
+### options.prefix
+
+Type: `String`
+
+Default: `gulp-simple-`
+
+Prefix for gulp tasks
+
+### options.minify
+
+Type: `Boolean`
+
+Default: `false`
+
+Enable minify. See `config.types[type].minify`
+
+
+## Sample
+
+Add to your project `gulpfile.js`:
+
+```
+const gulp = require('gulp');
+const runSequence = require('run-sequence');
+const minify = require('minimist');
+
+require('gulp-simple')({
+    src: 'source',
+    dest: 'build',
+    types: [
+        css: {
+            src: 'css/**/*.css',
+            dest: 'css',
+            pipe: [
+                [ "gulp-autoprefixer", "last 2 versions" ]
+            ,
+            minify: [
+                [ "gulp-cssnano" ]
+            ]
+        },
+        less: {
+            src: '**/*.less',
+            dest: 'css',
+            pipe: [
+                [ "gulp-less" ],
+                [ "gulp-autoprefixer", "last 2 versions" ]
+            ],
+            minify: [
+                [ "gulp-cssnano" ]
+            ]
+        },
+        js: {
+            src: '**/*.js',
+            dest: 'js',
+            minify: [
+                [ "gulp-uglify" ]
+            ]
+        }
+    ],
+    clean: [ 'build/**', '!build', '!build/uploads' ]
+}, {
+    minify: minimist(process.argv.slice(2)).release,
+});
+
+gulp.task('default', callback => runSequence(
+    'gulp-simple-clean',
+    'gulp-simple-build',
+    callback
+));
+
+gulp.task('watch', callback => runSequence(
+    'gulp-simple-clean',
+    'gulp-simple-build',
+    'gulp-simple-watch',
+    callback
+));
+
+```
---
+++ index.js
@@ -0,0 +1,101 @@
+'use strict';
+
+
+const gulp = require('gulp');
+const gutil = require('gulp-util')
+const watch = require('gulp-watch');
+const runSequence = require('run-sequence');
+
+const lazypipe = require('lazypipe');
+const filter = require('gulp-filter');
+const empty = require('gulp-empty-pipe');
+const plumber = require('gulp-plumber');
+
+const fs = require('fs');
+const path = require('path');
+const del = require('del');
+
+const logger = require('gulplog');
+
+
+module.exports = (config, options) => {
+    config.clean = config.clean || config.dest;
+    options.prefix = options.prefix || 'gulp-simple-';
+
+
+    function errorHandler(error) { gulplog.error(error.message) }
+
+    function createPipe(pipesRaw) {
+        if (!pipesRaw || !pipesRaw.length) return empty;
+
+        let result = lazypipe();
+        pipesRaw.forEach(pipeRaw => {
+            let pipeData = JSON.parse(JSON.stringify(pipeRaw));
+            pipeData[0] = require(pipeData[0]);
+            result = result.pipe.apply(result, pipeData);
+        });
+        return result;
+    }
+
+    function executeGulpTask({ src, dest, pipe, minify }, filePath) {
+        return gulp.src(src, { dot: true })
+            .pipe(filter(file => !filePath || file.path === filePath))
+            .pipe(plumber({ errorHandler }))
+            .pipe(pipe())
+            .pipe(options.minify ? minify() : empty())
+            .pipe(gulp.dest(dest));
+    }
+
+
+    config.types.etc = { src: [ '**/*' ] };
+
+    Object.keys(config.types).forEach(typeName => {
+        let type = config.types[typeName];
+        type.src = path.resolve(config.src, type.src);
+        type.dest = path.resolve(config.dest, type.dest || '');
+        type.pipe = createPipe(type.pipe);
+        type.minify = createPipe(type.minify);
+    });
+
+    Object.keys(config.types).forEach(typeName => {
+        if (typeName === 'etc') return;
+
+        let type = config.types[typeName];
+        config.types.etc.src.push(`!${type.src}`);
+    });
+
+
+    //=========================
+    //  Gulp tasks
+    //=========================
+
+    Object.keys(config.types).forEach(typeName => {
+        let type = config.types[typeName];
+        gulp.task(options.prefix + typeName, () => executeGulpTask(type));
+    });
+
+    gulp.task(options.prefix + 'build', callback => runSequence(
+        Object.keys(config.types).map(typeName => options.prefix + typeName),
+        callback
+    ));
+
+    gulp.task(options.prefix + 'clean', callback => del(config.clean, callback));
+
+    gulp.task(options.prefix + 'watch', callback => {
+        Object.keys(config.types).forEach(typeName => {
+            let type = config.types[typeName];
+            watch(type.src, vinyl => {
+                switch (vinyl.event) {
+                    case 'add':
+                    case 'change':
+                        executeGulpTask(type, vinyl.path).on('end', () => gulplog.info(`File "${path.relative('.', vinyl.path)}": ${vinyl.event} as "${typeName}"`));
+                        break;
+
+                    case 'unlink':
+                        gulplog.info(`Уou must restart gulp to delete file "${path.relative(config.src, vinyl.path)}"`);
+                        break;
+                }
+            }).on('error', errorHandler);
+        });
+    });
+};
